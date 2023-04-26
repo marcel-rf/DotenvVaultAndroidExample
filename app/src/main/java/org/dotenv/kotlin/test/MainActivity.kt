03b7c75861c70ac0edd79ba179e6b4d29a25e49d
marcel-rf <marcelr01@gmail.com>
2023-04-26 11:21:49 -0300

    Print decrypted variable value on MainActivity. Add maven local repo for dev changes. Add buildConfigField into gradle script to feed the vault decoding key either from the host env vars or from the local.properties file.

---
+++ app/src/main/java/org/dotenv/kotlin/test/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.dotenv.kotlin.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.dotenv.vault.dotenvVault
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val vaultKey = BuildConfig.DOTENV_KEY_DEVELOPMENT
+        println("initializing vault with key: ${vaultKey}")
+        val vault = dotenvVault(vaultKey) {
+            directory = "/assets"
+            filename = "env.vault"
+        }
+
+        val decryptedValue = vault["MY_TEST_EV1"]
+        println("dotenv decrypted value: ${decryptedValue}")
+    }
+}
--- app/src/test/java/org/dotenv/kotlin/test/EnvVaultExample.kt
+++ app/src/test/java/org/dotenv/kotlin/test/EnvVaultExample.kt
@@ -50,37 +50,30 @@ class EnvVaultExample {
         assertEquals(message, decryptedMessage)
     }
 
+    /**
+     *  $cat .env.keys
+        # DOTENV_KEYs (generated with npx dotenv-vault local build)
+        DOTENV_KEY_DEVELOPMENT="dotenv://:key_14968ef8b3f56cbcfcaa83197efa34dba567e715e82ee69e050258a5522100f6@dotenv.local/vault/env.vault?environment=development"
+        decryption key is: 14968ef8b3f56cbcfcaa83197efa34dba567e715e82ee69e050258a5522100f6
+
+        $cat env.vault
+        # env.vault (generated with npx dotenv-vault local build)
+        DOTENV_VAULT_DEVELOPMENT="BuEEZbR/YQKR2Rj/bHvOQbv+gtIxPoik1BCqpvIdqmv/lhLr5PAKs0r9iIZ4dBWNlQE7WdQ8vak00cQuEV5/tmRUkN1g/HFcRuJMN5slL+xuUzK5YJo1XYRwsdHvMG2BUGST"
+     *
+     */
     @Test
     fun verifyDecryptionOfVaultWithActualKey() {
         val key = "14968ef8b3f56cbcfcaa83197efa34dba567e715e82ee69e050258a5522100f6"
 
-        /**
-         * KEY
-         * marcel@aconcagua:~/dev/dotenv/testproject$ cat .env.keys
-            # DOTENV_KEYs (generated with npx dotenv-vault local build)
-            DOTENV_KEY_DEVELOPMENT="dotenv://:key_14968ef8b3f56cbcfcaa83197efa34dba567e715e82ee69e050258a5522100f6@dotenv.local/vault/.env.vault?environment=development"
-         */
-        /*
-            encrypted message in vault file:
-            # .env.vault (generated with npx dotenv-vault local build)
-            DOTENV_VAULT_DEVELOPMENT="BuEEZbR/YQKR2Rj/bHvOQbv+gtIxPoik1BCqpvIdqmv/lhLr5PAKs0r9iIZ4dBWNlQE7WdQ8vak00cQuEV5/tmRUkN1g/HFcRuJMN5slL+xuUzK5YJo1XYRwsdHvMG2BUGST"
-         */
         val encryptedMessage = "BuEEZbR/YQKR2Rj/bHvOQbv+gtIxPoik1BCqpvIdqmv/lhLr5PAKs0r9iIZ4dBWNlQE7WdQ8vak00cQuEV5/tmRUkN1g/HFcRuJMN5slL+xuUzK5YJo1XYRwsdHvMG2BUGST"
 
         val secretKey = createKeyFromBytes(key.fromHexString())
-
-//        val encryptedMessage = encrypt(secretKey, message)
         println("encryptedMessage ${encryptedMessage}")
-
-        //val decodedEncryptedMessage = encryptedMessage.toByteArray(charset("UTF-8"))
         val decodedEncryptedMessage = Base64.getDecoder().decode(encryptedMessage)
-
         val decryptedMessage = decrypt(secretKey, decodedEncryptedMessage)
         println("decryptedMessage ${decryptedMessage}")
         //assertEquals(message, decryptedMessage)
     }
-
-
 }
 
 
